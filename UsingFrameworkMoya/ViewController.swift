304defd4e3c00b69f7cc982eedf7df0c7ded1f69
Mirjaloliddin <125131812+Mirjaloliddin@users.noreply.github.com>
2023-09-14 12:53:01 +0500

    Initial Commit

---
+++ UsingFrameworkMoya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingFrameworkMoya
+//
+//  Created by Murtazaev Mirjaloliddin Kamolovich on 14/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
